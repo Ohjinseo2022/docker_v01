34d89861fbf07a03195c444a8ad1ea49dc4b7fc7
오진서 <dhwlstj@gmail.com>
2023-09-24 13:07:47 +0900

    도커배포 테스트 를 위한 첫번째 커밋

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = {
+    output: 'standalone'
+}
+
+module.exports = nextConfig
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div>
+      도커 빌드 테스트 중입니다 
+    </div>
+  )
+}
